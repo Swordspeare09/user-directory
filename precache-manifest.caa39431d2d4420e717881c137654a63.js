d82f86b1a7de1f985796f459ac3adef2d88c2d8a
Francisco J Cortez <cortez.francisco.j.2009@gmail.com>
2020-04-06 08:59:20 -0500

    Updates

---
+++ precache-manifest.caa39431d2d4420e717881c137654a63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a43e62b86ce1c85c5509a34088dae7",
+    "url": "/user-directory/index.html"
+  },
+  {
+    "revision": "2eb3f9a22d9714bbe9ff",
+    "url": "/user-directory/static/css/main.71953a1e.chunk.css"
+  },
+  {
+    "revision": "3d18496a92f176782cce",
+    "url": "/user-directory/static/js/2.5d5af43c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/user-directory/static/js/2.5d5af43c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eb3f9a22d9714bbe9ff",
+    "url": "/user-directory/static/js/main.fa5905af.chunk.js"
+  },
+  {
+    "revision": "60e6b4db63d0cc56d962",
+    "url": "/user-directory/static/js/runtime-main.caef5b4b.js"
+  }
+]);
